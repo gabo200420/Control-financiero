0e7df9cf933267e7cfbdbe540b328eae28c00b62
Gabriel <tu_correo@gmail.com>
2026-08-26 23:02:20 -0500

    Corregir reconexion SSL Neon

--- database.py
+++ database.py
@@ -11,7 +11,8 @@ DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///finanzas.db")
 
 engine = create_engine(
     DATABASE_URL,
-    connect_args={"check_same_thread": False} if "sqlite" in DATABASE_URL else {}
+    pool_pre_ping=True,
+    pool_recycle=300
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
